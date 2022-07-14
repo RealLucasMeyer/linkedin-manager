766272f41906933a13acb0c42d6aaeb84ee5e2c1
Lucas Meyer <lucas.meyer@microsoft.com>
2022-07-14 15:05:37 -0700

    Created calendar

--- app.py
+++ app.py
@@ -1,29 +1,60 @@
-from datetime import datetime
+from asyncio import FastChildWatcher
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 app = Flask(__name__)
+from azure.cosmos import CosmosClient
+import os
+from dotenv import load_dotenv
+import datetime
 
+def get_container_connection(db_name, container_name):
+
+    COSMOS_URI = os.environ["COSMOS_URI"]
+    COSMOS_KEY = os.environ["COSMOS_KEY"]
+
+    client = CosmosClient(COSMOS_URI, credential=COSMOS_KEY)
+
+    database = client.get_database_client(db_name)
+    container = database.get_container_client(container_name)
+
+    return container
+
+def convert_cosmos_utc_to_local(cosmos_utc_time: str) -> datetime:
+    input_time = cosmos_utc_time[:-2]
+    dtUTC = datetime.datetime.strptime(input_time, "%Y-%m-%dT%H-%M-%S.%f")
+    dtZone = dtUTC.replace(tzinfo = datetime.timezone.utc)
+    dtLocal = dtZone.astimezone()
+    return dtLocal
 
 @app.route('/')
 def index():
-   print('Request for index page received')
-   return render_template('index.html')
+    container = get_container_connection("social-media", "blog-posts")
+    q = 'SELECT c.title, c["linkedin-target-date-utc"] AS li_date,\
+        c["twitter-target-date-utc"] AS tw_date, c["draft"] FROM c\
+        WHERE c["linkedin-target-date-utc"] > GetCurrentDateTime() OR\
+        c["twitter-target-date-utc"] > GetCurrentDateTime()'
+    
+    posts = container.query_items(query=q, enable_cross_partition_query=True)
+    
+    item_list = []
+    for p in posts:
+        d = dict(p)
+        if p.get('li_date'):
+            d['li_date'] = str(convert_cosmos_utc_to_local(p['li_date']))[:19]
+        if p.get('tw_date'):
+            d['tw_date'] = str(convert_cosmos_utc_to_local(p['tw_date']))[:19]
+        if not p.get("draft"):
+            d['draft'] = False
+        item_list.append(d)        
+
+    item_list = sorted(item_list, key=lambda x: x["li_date"])
+    return render_template('index.html', posts=item_list) 
 
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
-
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
-
 
 if __name__ == '__main__':
-   app.run()
+    load_dotenv()
+    app.run()
